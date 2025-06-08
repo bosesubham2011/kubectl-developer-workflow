f67e48745e426b567fe2097b67d580ddc8deffc9
Madhur Tripathi <Madhur.Tripathi001@gmail.com>
2025-06-08 12:03:16 +0530

    Kubernetes Developer Conifguration

---
+++ k8sDevConfig.sh
@@ -0,0 +1,9 @@
+#1/bin/bash
+
+alias k = "kubectl"
+
+alias kp = "kubectl get pods"
+alias kpw = "kubectl get pods -o wide"
+alias kd = "kubectl get deployments"
+alias ksts = "kubectl get statefulsets"
+alias kn = "kubectl get nodes"
