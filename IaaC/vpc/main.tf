693ed154bc1ec335797a6537193ffbce4152a92c
Madhur Tripathi <Madhur.Tripathi001@gmail.com>
2025-06-08 14:37:52 +0530

    Debugging

--- IaaC/vpc/main.tf
+++ IaaC/vpc/main.tf
@@ -10,12 +10,12 @@ resource "aws_subnet" "priv_sub" {
     for_each = toset(var.priv_sub_cidr)
     vpc_id = aws_vpc.main_vpc.id
     cidr_block = each.value
-    availability_zone = data.aws_availability_zones.aws_available.names[index(var.priv_sub_cidr, each.value)]
+    availability_zone = data.aws_availability_zones.available.names[index(var.priv_sub_cidr, each.value)]
 }
 
 resource "aws_subnet" "pub_sub" {
     for_each = toset(var.pub_sub_cidr)
     vpc_id = aws_vpc.main_vpc.id
     cidr_block = each.value
-    availability_zone = data.aws_availability_zones.aws_available.names[index(var.pub_sub_cidr, each.value)]
+    availability_zone = data.aws_availability_zones.available.names[index(var.pub_sub_cidr, each.value)]
 }
